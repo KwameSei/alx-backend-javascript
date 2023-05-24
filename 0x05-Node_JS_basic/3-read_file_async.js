7d61ba1e4ee5385584a6940627ceef514ad81f76
KwameSei <nathanielosei92@gmail.com>
2023-05-24 18:32:57 +0000

    Reading files asynchrounously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const countStudents = (doc) => {
+  const filePath = path.join(__dirname, doc); // __dirname is the directory name of current module
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => { // readFileSync is a synchronous version of readFile
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').slice(1, data.length - 1); // split the data into lines
+        let count = 0; // count the number of lines
+        const fields = {}; // create an object to store the fields
+
+        for (const i of lines) { // iterate through the lines
+          if (i) { // if the line is not empty
+            const [student, , , field] = i.trim().split(','); // split the line into columns
+            if (!fields[field]) { // if the field is not yet in the fields object
+              fields[field] = []; // add the field to the fields object
+            }
+            fields[field].push(student); // add the student to the field
+            count += 1; // increment the number of lines
+          }
+        }
+
+        console.log(`Number of students: ${count}`); // print the number of lines
+        for (const field in fields) { // iterate through the fields
+          if (field) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
+        }
+
+        resolve();
+      }
+    });
+  });
+};
+
+module.exports = countStudents;
