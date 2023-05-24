86f54cfcd9de73d9474f6fa638aad4a51fc07a65
KwameSei <nathanielosei92@gmail.com>
2023-05-24 12:31:35 +0000

    Eslint problems fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  const user_input = process.stdin.read();  // Read user input
-  if (user_input !== null) {  // If user input is not empty
-    // const trimmed_input = user_input.trim();  // Trim any leading/trailing whitespace
+  const userInput = process.stdin.read(); // Read user input
+  if (userInput !== null) { // If user input is not empty
+    // const trimmed_input = user_input.trim(); // Trim any leading/trailing whitespace
     // if (trimmed_input === 'exit') {
     //   process.stdout.write('This important software is now closing\n');
-    //   process.exit();  // Terminate the program
+    //   process.exit(); // Terminate the program
     // } else {
-      process.stdout.write(`Your name is: ${user_input}\n`);
+    process.stdout.write(`Your name is: ${userInput}\n`);
   }
 });
 
