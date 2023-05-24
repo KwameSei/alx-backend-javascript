9762ef381141f5f42efdb19bf97526a63cb41ed6
KwameSei <nathanielosei92@gmail.com>
2023-05-24 12:41:42 +0000

    Unnecessary commentted out lines removed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,11 +4,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const userInput = process.stdin.read(); // Read user input
   if (userInput !== null) { // If user input is not empty
-    // const trimmed_input = user_input.trim(); // Trim any leading/trailing whitespace
-    // if (trimmed_input === 'exit') {
-    //   process.stdout.write('This important software is now closing\n');
-    //   process.exit(); // Terminate the program
-    // } else {
     process.stdout.write(`Your name is: ${userInput}\n`);
   }
 });
